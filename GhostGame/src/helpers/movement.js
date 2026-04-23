8f2545f450f996c432d9d95947e04afa87e546d8
Michael Cohagan <34299560+MSCohagan@users.noreply.github.com>
2026-04-22 21:44:15 -0600

    Setup movement for ghost and box object

--- GhostGame/src/helpers/movement.js
+++ GhostGame/src/helpers/movement.js
@@ -1,7 +1,32 @@
 export function applyGhostMovement( entity, controls, speed = 180) {
 
+    const body = entity.body
+    if(!body) return
+    
+    body.setVelocity(0, 0)
+
+    if(controls.left.isDown) body.setVelocityX(-this.speed)
+    if(controls.right.isDown) body.setVelocityX(this.speed)
+    if(controls.up.isDown) body.setVelocityY(-this.speed)
+    if(controls.down.isDown) body.setVelocity(this.speed)
 }
 
 export function applyPlatformerMovement( entity, controls, speed = 180, jumpVelocity = -300) {
     
+    const body = entity.body
+    if(!body) return
+
+    body.setVelocityX(0)
+
+    if (controls.left.isDown) {
+        body.setVelocityX(-this.speed)
+    } else if (controls.right.isDown) {
+        body.setVelocityX(this.speed)
+    }
+
+    const isOnFloor = body.blocked.down || body.touching.down
+
+    if (Phaser.Input.Keyboard.JustDown(this.jumpKey) && isOnFloor) {
+        body.setVelocityY(this.jumpVelocity)
+    }
 }
