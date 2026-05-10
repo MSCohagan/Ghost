142200fb2a7b2f20f3c0eaffc2f0d4149038e0c9
Michael Cohagan <ms.cohagan@gmail.com>
2026-05-10 02:01:30 -0600

    Removing test logs

--- GhostGame/src/controllers/gameplay/PuzzleController.js
+++ GhostGame/src/controllers/gameplay/PuzzleController.js
@@ -18,37 +18,16 @@ export default class PuzzleController {
   }
 
   setupPressurePlateEventsFor(pressurePlates) {
-    console.log(
-      '[PuzzleController] setupPressurePlateEventsFor count=',
-      pressurePlates?.length ?? 0
-    )
-
     pressurePlates.forEach((plate) => {
       if (plate._puzzleListenersBound) return
-      console.log('[PuzzleController] binding plate listeners', {
-        key: plate.key,
-        targetGate: plate.targetGate,
-        x: plate.x,
-        y: plate.y,
-      })
 
       plate.on('pressed', () => {
         const gate = this.findGateForPlate(plate)
-        console.log('[PuzzleController] plate pressed', {
-          plateKey: plate.key,
-          targetGate: plate.targetGate,
-          resolvedGate: gate?.key ?? null,
-        })
         gate?.open()
       })
 
       plate.on('released', () => {
         const gate = this.findGateForPlate(plate)
-        console.log('[PuzzleController] plate released', {
-          plateKey: plate.key,
-          targetGate: plate.targetGate,
-          resolvedGate: gate?.key ?? null,
-        })
         gate?.close()
       })
       plate._puzzleListenersBound = true
@@ -74,28 +53,9 @@ export default class PuzzleController {
   }
 
   update() {
-    if (!this._debugTick || this._debugTick % 120 === 0) {
-      console.log('[PuzzleController] update snapshot', {
-        gateCount: this.gates.length,
-        plateCount: this.pressurePlates.length,
-        possessableCount: this.possessables.length,
-      })
-    }
-    this._debugTick = (this._debugTick ?? 0) + 1
-
     this.pressurePlates.forEach((plate) => {
       const overlapHits = this.possessables.filter((obj) => this.scene.physics.overlap(obj, plate))
       const pressed = overlapHits.length > 0
-
-      if (pressed !== plate.isPressed) {
-        console.log('[PuzzleController] plate overlap state changed', {
-          plateKey: plate.key,
-          targetGate: plate.targetGate,
-          plateX: plate.x,
-          plateY: plate.y,
-          overlapHits: overlapHits.length,
-        })
-      }
 
       plate.setPressed(pressed)
     })
